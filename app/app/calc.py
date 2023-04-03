dbb0933cfaa9e1533081df62a0061e23dba53a65
rashuky <jakubrachucki@gmail.com>
2023-04-03 19:39:57 +0200

    Progress

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """Adds x and y and return results"""
+    return x + y
+
+
+def subtract(x, y):
+    """Substract x from y and return result"""
+    return y - x
